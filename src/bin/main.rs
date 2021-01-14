7c05155244b88a78c259f9ac1492de6b7a72238f
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-01-14 11:12:53 +0200

    Remove lexer code

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,14 +1,3 @@
-use eql::lexer;
-
 fn main() {
-  let res = lexer::lex("src/parser.rs:34:1");
-  res.map_or_else(
-    |err| println!("{}", err),
-    |tokens| {
-      tokens
-        .into_iter()
-        .map(|t| t.value)
-        .for_each(|t| println!("{}", t))
-    },
-  )
+  println!("Hello world");
 }
