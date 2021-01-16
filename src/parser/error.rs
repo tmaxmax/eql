c026f2fa710e7e8029316f4495b7634b5b2c4d4a
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-01-16 15:54:02 +0200

    Fix error display and remove redundant lifetimes.  - error now shows the unexpected token's line and column numbers if on the same line with the operation.  - fix padding off by one for unexpected token  - fix borrow checker error in fmt_expected  - rename expected_token to expected_tokens and make it clone on write

--- src/parser/error.rs
+++ src/parser/error.rs
@@ -4,12 +4,12 @@ use crate::util;
 use std::borrow::Cow;
 use std::fmt;
 
-#[derive(Debug)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Error<'a> {
   operation_kind: OperationKind,
   operation_token: lexer::Token<'a>,
   unexpected_token: Option<lexer::Token<'a>>,
-  expected_token: Option<&'static [lexer::TokenValue<'static>]>,
+  expected_tokens: Option<Cow<'static, [lexer::TokenValue<'static>]>>,
   details: Option<Cow<'static, str>>,
 }
 
@@ -18,20 +18,20 @@ impl<'a> Error<'a> {
     operation_kind: OperationKind,
     operation_token: lexer::Token<'a>,
     unexpected_token: Option<lexer::Token<'a>>,
-    expected_token: Option<&'static [lexer::TokenValue<'static>]>,
+    expected_tokens: Option<Cow<'static, [lexer::TokenValue<'static>]>>,
     details: Option<Cow<'static, str>>,
   ) -> Self {
     Error {
       operation_kind,
       operation_token,
       unexpected_token,
-      expected_token,
+      expected_tokens,
       details,
     }
   }
 }
 
-fn fmt_unexpected<'a>(e: &Error<'a>) -> String {
+fn fmt_unexpected(e: &Error) -> String {
   if let Some(un_token) = e.unexpected_token {
     let s = format!("Unexpected {}", un_token.value);
     if un_token != e.operation_token && un_token.line != e.operation_token.line {
@@ -46,7 +46,8 @@ fn fmt_unexpected<'a>(e: &Error<'a>) -> String {
     } else {
       let offset = un_token.column_number
         - e.operation_token.column_number
-        - util::string_length(e.operation_token.value.get());
+        - util::string_length(e.operation_token.value.get())
+        + 1;
       let (padding, pointer) = util::fmt_token_pointer(un_token.value.get(), offset);
       format!("{}{}\n{}", padding, pointer, s)
     }
@@ -55,9 +56,9 @@ fn fmt_unexpected<'a>(e: &Error<'a>) -> String {
   }
 }
 
-fn fmt_expected<'a>(e: &Error<'a>) -> String {
-  if let Some(ex_tokens) = e.expected_token {
-    let expected_list = util::fmt_list(ex_tokens, ", ", "or");
+fn fmt_expected(e: &Error) -> String {
+  if let Some(ex_tokens) = &e.expected_tokens {
+    let expected_list = util::fmt_list(&ex_tokens, ", ", "or");
     if expected_list.is_empty() {
       "".into()
     } else {
@@ -71,13 +72,22 @@ fn fmt_expected<'a>(e: &Error<'a>) -> String {
 impl fmt::Display for Error<'_> {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     let op_token = &self.operation_token;
+    let un_token = if let Some(t) = self.unexpected_token {
+      if t.line_number == op_token.line_number {
+        t
+      } else {
+        *op_token
+      }
+    } else {
+      *op_token
+    };
     let (padding, pointer) = util::fmt_token_pointer(op_token.value.get(), op_token.column_number);
     write!(
       f,
       "Error on {} operation on line {}, column {}:\n  {}\n  {}{}{}{}{}",
       self.operation_kind,
-      op_token.line_number,
-      op_token.column_number,
+      un_token.line_number,
+      un_token.column_number,
       op_token.line,
       padding,
       pointer,
