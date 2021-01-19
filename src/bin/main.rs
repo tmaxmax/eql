a6f7d04f44e2fde49aa14a5eaa2494b32700c26d
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-01-20 00:08:18 +0200

    Extract input retrieval to separate function

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,28 +3,29 @@ extern crate eql;
 use eql::lexer::last_token_value;
 use std::io::{self, BufRead};
 
+fn get_input(mut handle: impl BufRead) -> io::Result<String> {
+  let mut buf = String::new();
+  let mut total_read = 0;
+
+  loop {
+    let read = handle.read_line(&mut buf)?;
+    total_read += read;
+    if let Some(tail) = last_token_value(&buf[total_read - read..total_read].trim_end()) {
+      // FIXME: Break on actual terminators
+      if !matches!(tail, eql::lexer::Word(_)) {
+        break;
+      }
+    }
+  }
+
+  Ok(buf)
+}
+
 fn main() -> io::Result<()> {
   let stdin = io::stdin();
 
   loop {
-    let source = {
-      let mut buf = String::new();
-      let mut total_read = 0;
-      let mut handle = stdin.lock();
-
-      loop {
-        let read = handle.read_line(&mut buf)?;
-        total_read += read;
-        if let Some(tail) = last_token_value(&buf[total_read - read..total_read].trim_end()) {
-          // FIXME: Break on actual terminators
-          if !matches!(tail, eql::lexer::Word(_)) {
-            break;
-          }
-        }
-      }
-
-      buf
-    };
+    let source = get_input(stdin.lock())?;
 
     let operations = match eql::lex_parse(&source) {
       Ok(ops) => ops,
