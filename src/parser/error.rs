99fca5661a7b5805e4b6dc7a27b6a69da29e680b
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-01-19 22:08:10 +0200

    Implement std::error::Error trait for parser::Error, small refactors and changes:  - Don't write "instead" in the error output if there is no unexpected token provided  - Use Option methods instead of `if let`

--- src/parser/error.rs
+++ src/parser/error.rs
@@ -1,12 +1,13 @@
 use super::lexer;
-use super::operation::OperationKind;
+use crate::operation;
 use crate::util;
 use std::borrow::Cow;
+use std::error;
 use std::fmt;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Error<'a> {
-  operation_kind: OperationKind,
+  operation_kind: operation::OperationKind,
   operation_token: lexer::Token<'a>,
   unexpected_token: Option<lexer::Token<'a>>,
   expected_tokens: Option<Cow<'static, [lexer::TokenValue<'static>]>>,
@@ -15,7 +16,7 @@ pub struct Error<'a> {
 
 impl<'a> Error<'a> {
   pub fn new(
-    operation_kind: OperationKind,
+    operation_kind: operation::OperationKind,
     operation_token: lexer::Token<'a>,
     unexpected_token: Option<lexer::Token<'a>>,
     expected_tokens: Option<Cow<'static, [lexer::TokenValue<'static>]>>,
@@ -31,56 +32,55 @@ impl<'a> Error<'a> {
   }
 }
 
+impl error::Error for Error<'_> {}
+
 fn fmt_unexpected(e: &Error) -> String {
-  if let Some(un_token) = e.unexpected_token {
-    let s = format!("Unexpected {}", un_token.value);
-    if un_token != e.operation_token && un_token.line != e.operation_token.line {
-      let (padding, pointer) =
-        util::fmt_token_pointer(un_token.value.get(), un_token.column_number);
-      format!(
-        "\n{} on line {}, column {}:
+  e.unexpected_token
+    .filter(|t| *t != e.operation_token)
+    .map_or("".into(), |un_token| {
+      let s = format!("Unexpected {}", un_token.value);
+      if un_token.line != e.operation_token.line {
+        let (padding, pointer) =
+          util::fmt_token_pointer(un_token.value.get(), un_token.column_number);
+        format!(
+          "\n{} on line {}, column {}:
   {}
   {}{}",
-        s, un_token.line_number, un_token.column_number, un_token.line, padding, pointer
-      )
-    } else {
-      let offset = un_token.column_number
-        - e.operation_token.column_number
-        - util::string_length(e.operation_token.value.get())
-        + 1;
-      let (padding, pointer) = util::fmt_token_pointer(un_token.value.get(), offset);
-      format!("{}{}\n{}", padding, pointer, s)
-    }
-  } else {
-    "".into()
-  }
+          s, un_token.line_number, un_token.column_number, un_token.line, padding, pointer
+        )
+      } else {
+        let offset = un_token.column_number
+          - e.operation_token.column_number
+          - util::string_length(e.operation_token.value.get())
+          + 1;
+        let (padding, pointer) = util::fmt_token_pointer(un_token.value.get(), offset);
+        format!("{}{}\n{}", padding, pointer, s)
+      }
+    })
 }
 
 fn fmt_expected(e: &Error) -> String {
-  if let Some(ex_tokens) = &e.expected_tokens {
-    let expected_list = util::fmt_list(&ex_tokens, ", ", "or");
+  e.expected_tokens.as_ref().map_or("".into(), |ex_tokens| {
+    let expected_list = util::fmt_list(ex_tokens, ", ", "or");
     if expected_list.is_empty() {
       "".into()
     } else {
-      format!("\nExpected {} instead", expected_list)
+      format!(
+        "\nExpected {}{}",
+        expected_list,
+        e.unexpected_token.map_or("", |_| " instead")
+      )
     }
-  } else {
-    "".into()
-  }
+  })
 }
 
 impl fmt::Display for Error<'_> {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     let op_token = &self.operation_token;
-    let un_token = if let Some(t) = self.unexpected_token {
-      if t.line_number == op_token.line_number {
-        t
-      } else {
-        *op_token
-      }
-    } else {
-      *op_token
-    };
+    let un_token = self
+      .unexpected_token
+      .filter(|t| t.line_number == op_token.line_number)
+      .unwrap_or(*op_token);
     let (padding, pointer) = util::fmt_token_pointer(op_token.value.get(), op_token.column_number);
     write!(
       f,
@@ -93,11 +93,10 @@ impl fmt::Display for Error<'_> {
       pointer,
       fmt_unexpected(self),
       fmt_expected(self),
-      if let Some(text) = &self.details {
-        ["\n", text].join("")
-      } else {
-        "".into()
-      }
+      self
+        .details
+        .as_ref()
+        .map_or("".into(), |text| ["\n", text].join(""))
     )
   }
 }
