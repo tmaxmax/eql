c90f8365e5090b7595559e95675ffedd0e9ffda4
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-01-19 22:05:49 +0200

    Use lexer module

--- src/parser/constants.rs
+++ src/parser/constants.rs
@@ -1,17 +1,16 @@
-use crate::lexer;
+use crate::lexer::*;
 
-pub const KEYWORD_ADD: lexer::TokenValue = lexer::TokenValue::Word("Add");
-pub const KEYWORD_CREATE: lexer::TokenValue = lexer::TokenValue::Word("Create");
-pub const KEYWORD_REMOVE: lexer::TokenValue = lexer::TokenValue::Word("Remove");
-pub const KEYWORD_SHOW: lexer::TokenValue = lexer::TokenValue::Word("Show");
-pub const KEYWORDS: [lexer::TokenValue; 4] =
-  [KEYWORD_ADD, KEYWORD_CREATE, KEYWORD_REMOVE, KEYWORD_SHOW];
+pub const KEYWORD_ADD: TokenValue = Word("Add");
+pub const KEYWORD_CREATE: TokenValue = Word("Create");
+pub const KEYWORD_REMOVE: TokenValue = Word("Remove");
+pub const KEYWORD_SHOW: TokenValue = Word("Show");
+pub const KEYWORDS: [TokenValue; 4] = [KEYWORD_ADD, KEYWORD_CREATE, KEYWORD_REMOVE, KEYWORD_SHOW];
 
-pub const LINKER_AND: lexer::TokenValue = lexer::TokenValue::Word("and");
-pub const LINKER_TO: lexer::TokenValue = lexer::TokenValue::Word("to");
-pub const LINKER_FROM: lexer::TokenValue = lexer::TokenValue::Word("from");
+pub const LINKER_AND: TokenValue = Word("and");
+pub const LINKER_TO: TokenValue = Word("to");
+pub const LINKER_FROM: TokenValue = Word("from");
 
-pub const RESERVED: [lexer::TokenValue; 7] = [
+pub const RESERVED: [TokenValue; 7] = [
   KEYWORD_ADD,
   KEYWORD_CREATE,
   KEYWORD_REMOVE,
@@ -21,9 +20,8 @@ pub const RESERVED: [lexer::TokenValue; 7] = [
   LINKER_FROM,
 ];
 
-pub const SEPARATOR: lexer::TokenValue = lexer::TokenValue::Punctuation(".");
-pub const SEPARATOR_OVERWRITE: lexer::TokenValue = lexer::TokenValue::Punctuation("!");
-pub const SEPARATOR_FAIL_SILENTLY: lexer::TokenValue = lexer::TokenValue::Punctuation("?");
-pub const SEPARATOR_VALUES: lexer::TokenValue = lexer::TokenValue::Punctuation(",");
-pub const TERMINATORS: [lexer::TokenValue; 3] =
-  [SEPARATOR, SEPARATOR_OVERWRITE, SEPARATOR_FAIL_SILENTLY];
+pub const SEPARATOR: TokenValue = Punctuation(".");
+pub const SEPARATOR_OVERWRITE: TokenValue = Punctuation("!");
+pub const SEPARATOR_FAIL_SILENTLY: TokenValue = Punctuation("?");
+pub const SEPARATOR_VALUES: TokenValue = Punctuation(",");
+pub const TERMINATORS: [TokenValue; 3] = [SEPARATOR, SEPARATOR_OVERWRITE, SEPARATOR_FAIL_SILENTLY];
