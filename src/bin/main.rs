1844aacb6e4ec5b44feba796b8c37a07b1b64cbc
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-01-19 22:24:02 +0200

    Implement small REPL

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-  println!("Hello world");
+extern crate eql;
+
+use eql::lexer::last_token_value;
+use std::io::{self, BufRead};
+
+fn main() -> io::Result<()> {
+  let stdin = io::stdin();
+
+  loop {
+    let source = {
+      let mut buf = String::new();
+      let mut total_read = 0;
+      let mut handle = stdin.lock();
+
+      loop {
+        let read = handle.read_line(&mut buf)?;
+        total_read += read;
+        if let Some(tail) = last_token_value(&buf[total_read - read..total_read].trim_end()) {
+          if !matches!(tail, eql::lexer::Word(_)) {
+            break;
+          }
+        }
+      }
+
+      buf
+    };
+
+    let operations = match eql::lex_parse(&source) {
+      Ok(ops) => ops,
+      Err(e) => {
+        eprintln!("{}", e);
+        continue;
+      }
+    };
+    for (i, op) in operations.iter().enumerate() {
+      println!("{}: {:?}", i, op);
+    }
+  }
 }
